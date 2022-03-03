b92504762cbf9000017c0837e63ef79d8710b49b
Hoang Ancient <hoangdinhnho23@gmail.com>
2022-03-03 14:34:45 +0700

    Add XauDoiXung.cpp file

---
+++ XauDoiXung.cpp
@@ -0,0 +1,28 @@
+/**
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+string daoNguocXau(const string & s)
+{
+    string dao;
+    int n = s.length();
+    for (int i = n-1; i >= 0; i--)
+    {
+        dao += s[i];
+    }
+    return dao;
+}
+
+int main()
+{
+    string c;
+    cin >> c;
+    if (daoNguocXau(c) == c) cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
